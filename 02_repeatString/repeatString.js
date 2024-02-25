ce75f6c83f9b5a2e2305142001d1d96f04652fb2
Dan Fletcher <40371568+fletcher23@users.noreply.github.com>
2024-02-25 09:35:33 -0600

    Solutions to helloWorld and repeatString complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const numOfTimes = Math.floor(Math.random() * 30);
 
+const repeatString = function(word, numOfTimes) {
+    if (numOfTimes >= 0) {
+    return word.repeat(numOfTimes);
+    } else {
+        return "ERROR";
+    }
 };
-
+console.log(repeatString);
 // Do not edit below this line
 module.exports = repeatString;
